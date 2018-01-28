d0d17d1ff0b1fff8b0b0cb1a6cfcbe4903917634
Mike <ssbeefeater@gmail.com>
2018-01-28 03:05:22 +0200

    export new funtions

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 import Form from './Form';
 import Input from './Input';
 import Submit from './Submit';
+import withValidator from './lib/withValidator';
+import smartForm from './lib/smartForm';
+import smartInput from './lib/smartInput';
+import smartButton from './lib/smartButton';
 
 export {
     Form,
     Input,
     Submit,
+    smartForm,
+    smartInput,
+    withValidator,
 };
